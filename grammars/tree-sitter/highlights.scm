e1038c4b4b9554a9daeb0f69e7018f69c639fb56
oscarale <mail.aleyoscar.com>
2024-08-27 16:30:13 -0500

    initial grammar

---
+++ grammars/tree-sitter/highlights.scm
@@ -0,0 +1,15 @@
+(comment) @comment.line.beancount
+
+(account) @constant.language.beancount
+
+(date) @constant.numeric.date.beancount
+
+(tags_links) @string.quoted.double.beancount
+
+(txn) @keyword.operator.flag.beancount
+
+(string) @string.quoted.narration.beancount
+
+(amount) @constant.numeric.currency.beancount
+
+(cost_spec) @variable.other.currency.beancount
