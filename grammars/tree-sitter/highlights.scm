ce7aa5e47b16a497f6219a6bd9ebc5cdac3acf2f
oscarale <mail.aleyoscar.com>
2024-08-30 00:36:23 -0500

    feat: Initial syntax and scopes, thanks to @savetheclocktower

--- grammars/tree-sitter/highlights.scm
+++ grammars/tree-sitter/highlights.scm
@@ -1,15 +1,51 @@
+; DIRECTIVES
+; ==========
+
 [
-	(option)
-	(open)
-] @function.directive.beancount
+	"balance"
+	"event"
+	"option"
+	"open"
+	"close"
+	"price"
+	"plugin"
+	"custom"
+	"commodity"
+	"include"
+	"pad"
+	"note"
+	"document"
+	"poptag"
+	"pushtag"
+] @support.function.directive.beancount
 
-(comment) @comment.line.beancount
+; KEY/VALUES
+; ==========
 
-(account) @function.account.beancount
+(key) @variable.key.beancount
 
-(date) @constant.numeric.date.beancount
+; COMMENTS
+; ========
 
-(txn) @keyword.operator.flag.beancount
+(comment) @comment.line.semicolon.beancount
+(tag) @comment.line.hash.tag.beancount
+(tags_links (link) @comment.line.carrot.link.beancount)
+
+; ACCOUNTS
+; ========
+
+(account) @support.other.function.account.beancount
+
+((account) @constant.language.beancount
+	(#set! adjust.endBeforeFirstMatchOf ":"))
+((account) @punctuation.separator.beancount
+	(#set! adjust.startAndEndAroundFirstMatchOf ":"))
+
+((account) @variable.account.beancount
+	(#set! adjust.startAfterFirstMatchOf ":"))
+
+; STRINGS
+; =======
 
 [
 	(string)
@@ -17,8 +53,29 @@
 	(payee)
 ] @string.quoted.beancount
 
-(amount) @constant.numeric.currency.beancount
+; NUMBERS
+; =======
 
-(currency) @keyword.currency.beancount
+;(number) @constant.numeric.currency.beancount
+(date) @constant.numeric.date.beancount
+(bool) @constant.boolean.beancount
 
-(number) @value.amount.beancount
+; OPERATORS
+; =========
+
+[
+	(at)
+	(atat)
+	(slash)
+	(plus)
+	(minus)
+	(asterisk)
+] @keyword.operator.assignment.price.beancount
+
+(txn) @keyword.operator.flag.beancount
+
+; MISC
+; ====
+
+(currency) @keyword.other.unit.currency.beancount
+(headline) @entity.name.section.beancount
